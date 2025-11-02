ff965cda65200b691556fe7d36b25e92b622c17d
LamerH <167861316+LamerH@users.noreply.github.com>
2025-11-02 18:57:58 +0800

    Add files via upload

---
+++ Web-lab5/geolocation.js
@@ -0,0 +1,19 @@
+//Function to get and display geolocation
+function getGeolocation() {
+    const x = document.getElementById("geolocation");
+    
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                x.innerHTML = "<strong>Latitude</strong>: " + position.coords.latitude +
+                             "<br><strong>Longitude</strong>: " + position.coords.longitude;
+            },
+            (error) => {
+                x.innerHTML = "There are some errors that make it impossible to get the geolocation.";
+            }
+        );
+    } else {
+        x.innerHTML = "There are some errors that make it impossible to get the geolocation.";
+    }
+}
+*/
